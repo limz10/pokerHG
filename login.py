dcb86220978907c63cd77de56ecd69a7515f8b0c
Thomas Li <5567900+limz10@users.noreply.github.com>
2023-03-16 21:11:58 -0700

    Create login.py

---
+++ login.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, session, redirect, url_for, render_template
+from database import connect_db
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as ' + session['username']
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+        user = cursor.fetchone()
+        conn.close()
+        if user is not None:
+            session['username'] = user[1]
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', error='Invalid username or password')
+    else:
+        return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('login'))
+    else:
+        return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
