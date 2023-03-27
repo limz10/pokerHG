30fb8727a37421eb0b6e19d30022501bfcd45645
Thomas Li <limz.li.10@gmail.com>
2023-03-27 15:49:58 -0700

    login page bootstrap

--- front_end/pokerhg/src/pages/_app.tsx
+++ front_end/pokerhg/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app';
 import { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+
 
 function App({ Component, pageProps }: AppProps) {
   const [render, setRender] = useState(false);
